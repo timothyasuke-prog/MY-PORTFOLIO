bcf0a5c254d0b9eef3a4575c1651ee13f26fa6db
Timothy Asuke <timothyasuke@example.com>
2026-04-29 01:41:26 +0300

    fix

---
+++ tmp-check.js
@@ -0,0 +1,4 @@
+const projects = require('./api/projects.js');
+const w = require('./api/projects/currently/working-on.js');
+console.log('projects', typeof projects);
+console.log('working', typeof w);
